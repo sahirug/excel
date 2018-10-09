d708d6b003eb9e654b493b334149f235e5ede048
Sahiru Gunawardene <sahirug@gmail.com>
2018-10-10 01:29:42 +0530

    Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 import java.io.File;
 import java.io.IOException;
+import java.text.MessageFormat;
 
 public class Main {
+
+    private final static String EXCEL_PATH = "C:\\Users\\sahir\\Documents\\Macros\\MacroWS.xlsm";
+
+    private final static String MACRO_NAME = "NewMacro2";
+
+    private final static String FILE_NAME = "MacroWS.xlsm";
+
     public static void main(String[] args) {
-        String cmd = "wscript " + new File(Main.class.getClassLoader().getResource("myvbs.vbs").getFile()).getAbsolutePath();
+        String path = new File(Main.class.getClassLoader().getResource("myvbs.vbs").getFile()).getAbsolutePath();
+        String cmd = MessageFormat.format("wscript {0} {1} {2} {3}", path, EXCEL_PATH, FILE_NAME, MACRO_NAME);
+
+//        String cmd = "wscript " + path;
         test2(cmd);
     }
 
@@ -11,14 +22,14 @@ public class Main {
         /*
          * PUT DLL FILE TO => C:\Program Files\Java\jdk1.8.0_171\jre\bin
          **/
-        File file = new File("C:\\Users\\sahir\\Documents\\Macros\\MacroWS.xlsm");
-        String macroName = "NewMacro2";
-        Excel.callExcelMacro(file, macroName);
+        File file = new File(EXCEL_PATH);
+        Excel.callExcelMacro(file, MACRO_NAME);
     }
 
     private static void test2(String cmd) {
 //      cmd = "wscript C:/Users/sahir/Documents/Macros/myvbs.vbs";
         try {
+            System.out.println("RUNNING COMMAND ===> " + cmd);
             Runtime.getRuntime().exec(cmd);
             System.out.println("=================================== ALL DONE ===========================================");
         } catch (IOException e) {
--- target/classes/myvbs.vbs
+++ target/classes/myvbs.vbs
@@ -1,7 +1,12 @@
-Set objExcel = CreateObject("Excel.Application")
-Set objWorkbook = objExcel.Workbooks.Open("C:\Users\sahir\Documents\Macros\MacroWS.xlsm")
+path = WScript.Arguments.Item(0)
+fileName = WScript.Arguments.Item(1)
+macroName = WScript.Arguments.Item(2)
 
-objExcel.Application.Run "MacroWS.xlsm!NewMacro2"
+Set objExcel = CreateObject("Excel.Application")
+Set objWorkbook = objExcel.Workbooks.Open(path)
+
+' objExcel.Application.Run "MacroWS.xlsm!NewMacro2"
+objExcel.Application.Run fileName + "!" + macroName
 objWorkbook.Save
 objExcel.DisplayAlerts = False 
 objExcel.ActiveWorkbook.Close
